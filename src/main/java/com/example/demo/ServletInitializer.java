7afe46eafc76005dadf3060a61618df595b0d141
Laxi4u <85995291+Laxi4u@users.noreply.github.com>
2023-09-04 22:51:14 +0530

    Update ServletInitializer.java

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SampleWebApplication.class);
+		return applicatio.sources(SampleWebApplication.class);
 	}
 
 }
